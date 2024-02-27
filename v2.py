64ac863cbb30a312b2a410f246fb5602dbb72266
Gerald Freislich <freislichg@gmail.com>
2024-02-27 14:59:13 +1030

    safe

--- v2.py
+++ v2.py
@@ -54,59 +54,46 @@ def bfs(n: int,
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     # Initialize the queue with the start position and the path
-    queue = deque([(start[0], start[1], 0, [(start[0], start[1])])])
+    queue = deque([(start[0], start[1], int(mapple[start[0]][start[1]]), [(start[0], start[1])])])
     visited = list(start)
     min_cost = float('inf')
     min_path = []
+    cost_list = list()
+    path_list = list()
     rows = n; cols = m;
-
+    counter = 0
     while queue:
-        print(queue)
+        # if(counter == pow(2, n*m)): break;
+        # counter += 1
         row, col, cost, path = queue.popleft()
-
+        # print((row, col, cost))
         if row == end[0] and col == end[1]:
             if cost < min_cost:
                 min_cost = cost
                 min_path = path
+                # print(min_cost)
             continue
         
         current_possibles = list()
         for d_row, d_col in directions:
             new_row, new_col = row + d_row, col + d_col
 
-            if 0 <= new_row < rows and 0 <= new_col < cols and mapple[new_row][new_col] != 'X' and (new_row, new_col) not in visited and (new_row, new_col) not in path:
+
+            if 0 <= new_row < rows and 0 <= new_col < cols and mapple[new_row][new_col] != 'X' and (new_row, new_col) not in path:
                 new_cost = cost + int(mapple[new_row][new_col])
                 new_path = path + [(new_row, new_col)]
                 queue.append((new_row, new_col, new_cost, new_path))
-                # visited.append((new_row, new_col))
-            
-                current_possibles.append((new_row, new_col, new_cost, new_path))
-        
-        if(len(current_possibles) > 0):
-            print("True")
-            min_one = sorted(current_possibles, key=lambda x:x[2], reverse=False)
-            queue.append(min_one[0])
-            visited.append((min_one[0][0], min_one[0][1]))
-        else:
-            print("Not True")
-            for d_row, d_col in directions:
-                new_row, new_col = row + d_row, col + d_col
-
-                if 0 <= new_row < rows and 0 <= new_col < cols and mapple[new_row][new_col] != 'X' and (new_row, new_col) not in visited:
-                    new_cost = cost + int(mapple[new_row][new_col])
-                    new_path = path + [(new_row, new_col)]
-                    queue.append((new_row, new_col, new_cost, new_path))
-                    visited.append((new_row, new_col))
-                
-                    current_possibles.append((new_row, new_col, new_cost, new_path))
 
     # Mark the path on the grid with asterisks
-    for r, c in min_path:
-        mapple[r][c] = '*'
+    if(len(min_path) >= 1):
+        for r, c in min_path:
+            mapple[r][c] = '*'
 
-    # Print the grid with the path
-    for row in mapple:
-        print(' '.join(map(str, row)))
+        # Print the grid with the path
+        for row in mapple:
+            print(' '.join(map(str, row)))
+    else:
+        print("null")
 
 
 
