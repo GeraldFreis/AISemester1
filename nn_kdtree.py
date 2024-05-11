03ef4e1f70452e2e8a67398a9fe2522e4aff9166
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2024-05-11 13:06:07 +0930

    safe

--- nn_kdtree.py
+++ nn_kdtree.py
@@ -20,7 +20,7 @@ def build_tree(points: list):...
 
 # then I want to be able to classify each point
 
-def classify_point(root, point: list)->None:...
+def classify_point(root, point: list)->tuple:...
 
 """Okay so Now I am going to actually write the functions because I am lazy lol"""
 
@@ -43,11 +43,12 @@ def read_in(filename: str)->list:
 
 """Need a node class"""
 
-class Node():
-    def __init__(self, d, val, point):
-        self.d = d
-        self.val = val
+class Node:
+    def __init__(self, point=None, label=None, left=None, right=None):
         self.point = point
+        self.label = label
+        self.left = left
+        self.right = right
     
 # def median(l: list)->tuple:
 #     # I want to take a list and not only return the median but also the index
@@ -60,75 +61,78 @@ def euclidean_distance(node_a, node_b)->float:
     return dist
     
 
-def build_tree(points: list, depth: int = 0):
-    """This takes in a list of points and builds a kdtree from it
-    the KDtree is represented"""
+def make_tree(points:list, labels:list, depth=0):
+    """Takes in a list of points and their respective labels and returns the root node"""
+    if len(points) == 0: # if we don't have any points
+        return None
 
-    if len(points) == 0: return None
-    elif len(points) == 1:
-        d = depth % dimensions
-        val = points[0][d] # getting value at dimension
-        new_node = Node(d, val, points[0])
-        new_node.left = None; new_node.right = None
-        return new_node
-    else:
-        d = depth % dimensions
-        points.sort(key=lambda x: x[d])
-        middle = len(points) // 2
+    d = depth % len(points[0]) # modding current depth by M
 
-        val = points[middle][d]# median value at the d'th dimension
-        
-        new_node = Node(d, val, points[middle]) # adding the new node with info
-        
-        new_node.left = build_tree([p for p in points if p[d] < val], depth+1)
-        new_node.right = build_tree([p for p in points if p[d] > val], depth+1)
+    # Sort points by the cut (d)
+    sorted_points = sorted(zip(points, labels), key=lambda x: x[0][d]) # I want to sort both together
+    # because otherwise the order of their labels will not be maintained
+    m = len(points) // 2
 
-        return new_node
+    # getting all points to the left of the median
+    left_node = make_tree([x[0] for x in sorted_points[:m]], [x[1] for x in sorted_points[:m]], depth + 1)
+    right_node = make_tree([x[0] for x in sorted_points[m+1:]], [x[1] for x in sorted_points[m+1:]], depth + 1)
 
-def classify_point(root, point: list, depth, best_thus_far = None):
+
+    # Create node and construct subtrees
+    node = Node(
+        point=sorted_points[m][0],
+        label=sorted_points[m][1],
+        left=left_node,
+        right=right_node
+    )
+    return node
+
+def classify_point(root, point: list, depth=0)->tuple:
     """Here we take a root node (from a KD tree) and a given point (list) and classify it
     based on its 1nn
+    i.e. we return its distance to the next value (which we need to check) and that node's's label
     """
-    if root is None: return best_thus_far
+    if root is None: return (float('inf'), None)
 
     d = depth % dimensions
 
     dist_sq = euclidean_distance(point, root)
-    dx = (point[d] - root.point[d]) ** 2
+    dx = sum(float((point[i] - root.point[i]) ** 2) 
+            for i in range(len(point)) )
+    dimension_distance = (point[d] - root.point[d]) ** 2 # distance between current node and point along dimension d
 
-    if(best_thus_far != None):
-        best_dist = euclidean_distance(point, best_thus_far)
-    else:
-        best_dist = dist_sq
-        best_thus_far = root
 
-    if(best_dist > dist_sq):
-        best_dist = dist_sq
-        best_thus_far = root
-
-    if point[d] < root.point[d]:
-        next_branch = root.left
-        opp_branch = root.right
-    else:
-        next_branch = root.right
-        opp_branch = root.left
-
-    best_thus_far = classify_point(next_branch, point, depth + 1, best_thus_far)
-
-    if dx < best_dist:
-        best_thus_far = classify_point(opp_branch, point, depth + 1, best_thus_far)
-
-    return best_thus_far
+    if(root.point[d] < point[d]): # if the current node's val along dimension d is < point we go right
+        n_b = root.right 
+        o_b = root.left
+    else: # if the current node val along dimension d > point we go left
+        n_b = root.left 
+        o_b = root.right
+    
+    dist, label = classify_point(n_b, point, depth+1)  # checking out the best branch first
+    if dx < dist: # if the current distance is better than the next branch's distance
+        dist = dx
+        label = root.label
+    
+    if(dimension_distance < dist):
+        d, l = classify_point(o_b, point, depth+1) # checking out the classification on the other branch
+        if(d < dist): # if the new distance is better than the last best
+            dist = d
+            label = l
+    
+    return (dist, label)
 
 train_file = read_in(train_filename)
-train_root = build_tree(train_file)
+train_points = [i[:-1] for i in train_file]
+train_labels = [i[-1] for i in train_file]
+train_root = make_tree(train_points, train_labels)
 
 
 
 test_file = read_in(test_filename)
 for i in test_file:
 
-    print(int(classify_point(train_root, i, 0, None).point[-1]))
+    print(int(classify_point(train_root, i, 0)[1]))
 
 
 
